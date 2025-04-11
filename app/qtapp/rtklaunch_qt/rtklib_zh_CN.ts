2bc872cf3819e9601903aaa646267e5e801bad2f
liusheng20241010 <liusheng@smail.xtu.edu.cn>
2025-04-11 23:32:27 +0800

    Added Chinese translation file rtklib_zh_CN.ts to 8 QT apps

---
+++ app/qtapp/rtklaunch_qt/rtklib_zh_CN.ts
@@ -0,0 +1,251 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN" sourcelanguage="en">
+<context>
+    <name>LaunchOptDialog</name>
+    <message>
+        <location filename="launchoptdlg.ui" line="14"/>
+        <source>Options</source>
+        <translation>选项</translation>
+    </message>
+    <message>
+        <location filename="launchoptdlg.ui" line="23"/>
+        <source>RTKPost_&amp;MKL, RTKNavi_MKL</source>
+        <translation>RTKPost_&amp;MKL, RTKNavi_MKL</translation>
+    </message>
+    <message>
+        <location filename="launchoptdlg.ui" line="33"/>
+        <source>RTKPost_WIN&amp;64, RTKNavi_WIN64</source>
+        <translation>RTKPost_WIN&amp;64, RTKNavi_WIN64</translation>
+    </message>
+    <message>
+        <location filename="launchoptdlg.ui" line="40"/>
+        <source>Minimize</source>
+        <translation>最小化</translation>
+    </message>
+    <message>
+        <location filename="launchoptdlg.ui" line="47"/>
+        <source>&amp;Normal APs (without MKL)</source>
+        <translation>&amp;普通应用程序（无 MKL）</translation>
+    </message>
+</context>
+<context>
+    <name>MainForm</name>
+    <message>
+        <location filename="launchmain.ui" line="14"/>
+        <source>RTKLib</source>
+        <translation>RTKLib</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="48"/>
+        <source>RTKLIB APs</source>
+        <translation>RTKLIB 应用程序</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="59"/>
+        <source>Exit</source>
+        <translation>退出</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="101"/>
+        <source>Task Tray</source>
+        <translation>任务栏</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="135"/>
+        <source>Plotting</source>
+        <translation>绘图</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="158"/>
+        <source>Data format converter</source>
+        <translation>数据格式转换器</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="181"/>
+        <source>Stream Server</source>
+        <translation>流服务器</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="204"/>
+        <source>Post Processing</source>
+        <translation>后处理</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="227"/>
+        <source>NTRIP Browser</source>
+        <translation>NTRIP 浏览器</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="250"/>
+        <source>Real-time Navigation</source>
+        <translation>实时导航</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="273"/>
+        <source>Data downloader</source>
+        <translation>数据下载器</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="297"/>
+        <source>rtkConv</source>
+        <translation>rtkConv</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="300"/>
+        <source>RTK Convert</source>
+        <translation>RTK 转换</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="309"/>
+        <source>rtkGet</source>
+        <translation>rtkGet</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="312"/>
+        <source>RTK Get</source>
+        <translation>RTK 获取</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="321"/>
+        <source>rtkNavi</source>
+        <translation>rtkNavi</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="324"/>
+        <source>RTK Navi</source>
+        <translation>RTK 导航</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="333"/>
+        <source>rtkNtrip</source>
+        <translation>rtkNtrip</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="336"/>
+        <source>RTK NTRIP</source>
+        <translation>RTK NTRIP</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="345"/>
+        <source>rtkPlot</source>
+        <translation>rtkPlot</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="348"/>
+        <source>RTK Plot</source>
+        <translation>RTK 绘图</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="357"/>
+        <source>rtkPost</source>
+        <translation>rtkPost</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="360"/>
+        <source>RTK Post</source>
+        <translation>RTK 后处理</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="369"/>
+        <source>rtkStr</source>
+        <translation>rtkStr</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="372"/>
+        <source>RTK Stream</source>
+        <translation>RTK 流</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="377"/>
+        <source>rtkVideo</source>
+        <translation>rtkVideo</translation>
+    </message>
+    <message>
+        <location filename="launchmain.ui" line="380"/>
+        <source>RTK Video</source>
+        <translation>RTK 视频</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="49"/>
+        <location filename="launchmain.cpp" line="58"/>
+        <source>RTKLIB Version %1 %2</source>
+        <translation>RTKLIB 版本 %1 %2</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="61"/>
+        <source>RTKLIB application launcher Qt</source>
+        <translation>RTKLIB 应用程序启动器 Qt</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="93"/>
+        <location filename="launchmain.cpp" line="110"/>
+        <source>&amp;Expand</source>
+        <translation>&amp;展开</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="95"/>
+        <source>RTK&amp;PLOT</source>
+        <translation>RTK&amp;绘图</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="96"/>
+        <source>&amp;RTKPOST</source>
+        <translation>&amp;RTK后处理</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="97"/>
+        <source>RTK&amp;NAVI</source>
+        <translation>RTK&amp;导航</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="98"/>
+        <source>RTK&amp;CONV</source>
+        <translation>RTK&amp;转换</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="99"/>
+        <source>RTK&amp;GET</source>
+        <translation>RTK&amp;获取</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="100"/>
+        <source>RTK&amp;STR</source>
+        <translation>RTK&amp;流</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="101"/>
+        <source>&amp;NTRIP BROWSER</source>
+        <translation>&amp;NTRIP 浏览器</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="103"/>
+        <location filename="launchmain.cpp" line="121"/>
+        <source>E&amp;xit</source>
+        <translation>退&amp;出</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <location filename="launchmain.cpp" line="66"/>
+        <source>Set Window Title</source>
+        <translation>设置窗口标题</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="67"/>
+        <source>title</source>
+        <translation>标题</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="71"/>
+        <source>Only show tray icon</source>
+        <translation>仅显示托盘图标</translation>
+    </message>
+    <message>
+        <location filename="launchmain.cpp" line="75"/>
+        <source>Start minimized</source>
+        <translation>启动时最小化</translation>
+    </message>
+</context>
+</TS>
