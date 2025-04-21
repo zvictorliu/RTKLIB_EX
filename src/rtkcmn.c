35dfb174afb301ccd9d6e80d6ec73a1931bfc412
rtklibexplorer <tim.everett3@gmail.com>
2025-04-21 17:16:37 -0600

    Move Beidou B1C from RTKLIB processing freq1 to freq6 to avoid conflict with B2I observations when both are present

--- src/rtkcmn.c
+++ src/rtkcmn.c
@@ -678,8 +678,8 @@ static int code2freq_BDS(uint8_t code, double *freq)
     char *obs=code2obs(code);
 
     switch (obs[0]) {
-        case '1': *freq=FREQL1;    return 0; /* B1C */
-        case '2': *freq=FREQ1_CMP; return 0; /* B1I */
+        case '1': *freq=FREQL1;    return 5; /* B1C */
+        case '2': *freq=FREQ1_CMP; return 0; /* B2I */
         case '7': *freq=FREQ2_CMP; return 1; /* B2b */
         case '5': *freq=FREQL5;    return 2; /* B2a */
         case '6': *freq=FREQ3_CMP; return 3; /* B3 */
